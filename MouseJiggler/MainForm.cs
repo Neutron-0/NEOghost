264d8b7a096937e388a8afecc6456ac864e124cd
Neutron <neutron275u@gmail.com>
2021-12-17 08:36:57 -0800

    Revert "Fixed startup jiggle"
    This reverts commit c2da0de8027dc52c5b239b5bd43e2c32b39c8ee1.

--- MouseJiggler/MainForm.cs
+++ MouseJiggler/MainForm.cs
@@ -47,7 +47,6 @@ namespace ***REMOVED***.MouseJiggler
         {
             if (this.JiggleOnStartup)
                 this.cbJiggling.Checked = true;
-                this.exitTrayMenuItem.Checked = true;
         }
 
         private void UpdateNotificationAreaText ()
